e99615abbafcf8f19254c2d2c155549e7fdb25b9
MS_D_rei <msmdmission1111@gmail.com>
2023-11-21 23:32:45 +0900

    First commit

---
+++ app/root.jsx
@@ -0,0 +1,20 @@
+import { Links, LiveReload, Meta, Outlet, Scripts } from "@remix-run/react";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="data:image/x-icon;base64,AA" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <h1>Hello World!</h1>
+        <Outlet />
+
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
