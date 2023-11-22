fa44e6eaf57a859fd18e94be10d12221889fbcf8
MS_D_rei <msmdmission1111@gmail.com>
2023-11-22 21:53:53 +0900

    Add: action function to app/routes/contacts.$contactId_.edit.tsx

--- app/routes/contacts.$contactId_.edit.tsx
+++ app/routes/contacts.$contactId_.edit.tsx
@@ -1,16 +1,33 @@
-import type { LoaderFunctionArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
+import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
 
-import { getContact } from "~/data";
+import { getContact, updateContact } from "~/data";
+
+export const action = async ({ params, request }: ActionFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId");
+
+  const formData = await request.formData();
+  const updates = Object.fromEntries(formData);
+  // This console.log shows up in the terminal, not browser console
+  console.log("updates", updates);
+  await updateContact(params.contactId, updates);
+
+  return redirect(`/contacts/${params.contactId}`);
+};
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   invariant(params.contactId, "Missing contactId");
+
+  // This console.log shows up in the terminal, not browser console
+  console.log("params", params);
+
   const contact = await getContact(params.contactId);
   if (!contact) {
     throw new Response("Not Found", { status: 404 });
   }
+
   return json({ contact });
 };
 
