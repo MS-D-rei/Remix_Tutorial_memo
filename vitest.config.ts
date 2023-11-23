e3b609407acc73eef372abd5baffbe5912b631ca
MS_D_rei <msmdmission1111@gmail.com>
2023-11-23 20:47:23 +0900

    ADD: enviornment: "happy-dom" to vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, configDefaults } from "vitest/config";
 
 export default defineConfig({
   test: {
+    environment: "happy-dom",
     exclude: [...configDefaults.exclude],
     watchExclude: [...configDefaults.watchExclude],
   },
