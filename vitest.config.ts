953c5aa6a815829264006ffefc4a19425047494d
MS_D_rei <msmdmission1111@gmail.com>
2023-11-24 22:20:59 +0900

    ADD: react plugin and tsconfigPaths plugin to vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,15 @@
 /// <reference types="vitest">
+
 import { defineConfig, configDefaults } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
   test: {
     environment: "happy-dom",
+    setupFiles: ["./test/setup-test-env.ts"],
+    include: ["./test/**/*.{test,spec}.{js,jsx,ts,tsx}"],
     exclude: [...configDefaults.exclude],
     watchExclude: [...configDefaults.watchExclude],
   },
