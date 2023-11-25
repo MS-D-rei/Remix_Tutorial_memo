9f16dc0276b6322de48e1682f2936ab1229f32db
MS_D_rei <msmdmission1111@gmail.com>
2023-11-25 21:24:44 +0900

    ADD: test case for loader func to test/root.test.tsx

--- test/root.test.tsx
+++ test/root.test.tsx
@@ -1,10 +1,12 @@
-import { describe, it, expect, afterEach } from "vitest";
+import { describe, it, expect, afterEach, vi, afterAll } from "vitest";
 import { render, screen, cleanup, waitFor } from "@testing-library/react";
 // import { RouterProvider, createMemoryRouter } from "react-router-dom";
-import App from "~/root";
+import App, { loader } from "~/root";
 import type { ContactRecord } from "~/data";
 import { json } from "@remix-run/node";
 import { createRemixStub } from "@remix-run/testing";
+
+import * as dataUtils from "~/data";
 
 afterEach(() => {
   cleanup();
@@ -56,7 +58,77 @@ describe("root", () => {
     render(<RemixStub />);
 
     await waitFor(() => screen.getByText("Remix Contacts"));
-    screen.debug();
+    // screen.debug();
     expect(screen.getByText("Remix Contacts")).toBeInTheDocument();
   });
 });
+
+describe("root loader", async () => {
+  afterAll(async () => {
+    // vi.restoreAllMocks();
+    getContactsSpy.mockRestore();
+  });
+
+  const fakeContacts: ContactRecord[] = [
+    {
+      id: "john-doe",
+      first: "John",
+      last: "Doe",
+      twitter: "@John-Doe",
+      createdAt: Date.now().toString(),
+    },
+    {
+      id: "jane-doe",
+      first: "Jane",
+      last: "Doe",
+      twitter: "@Jane-Doe",
+      createdAt: Date.now().toString(),
+    },
+  ];
+
+  const getContactsSpy = vi.spyOn(dataUtils, "getContacts");
+  getContactsSpy.mockResolvedValue(fakeContacts);
+
+  const response = await loader();
+  it("calls getContacts", async () => {
+    expect(getContactsSpy).toHaveBeenCalled();
+  });
+  it("returns response 200", async () => {
+    expect(response.status).toBe(200);
+  });
+  it("returns contacts as json", async () => {
+    expect(await response.json()).toEqual({ contacts: fakeContacts });
+  });
+});
+
+/* a response content of loader function
+_Response [Response] {
+  [Symbol(realm)]: { settingsObject: {} },
+  [Symbol(state)]: {
+    aborted: false,
+    rangeRequested: false,
+    timingAllowPassed: false,
+    requestIncludesCredentials: false,
+    type: 'default',
+    status: 200,
+    timingInfo: null,
+    cacheState: '',
+    statusText: '',
+    headersList: _HeadersList {
+      cookies: null,
+      [Symbol(headers map)]: [Map],
+      [Symbol(headers map sorted)]: null
+    },
+    urlList: [],
+    body: {
+      stream: undefined,
+      source: '{"contacts":[{"id":"john-doe","first":"John","last":"Doe","twitter":"@John-Doe","createdAt":"1700913014711"},{"id":"jane-doe","first":"Jane","last":"Doe","twitter":"@Jane-Doe","createdAt":"1700913014711"}]}',
+      length: 206
+    }
+  },
+  [Symbol(headers)]: _HeadersList {
+    cookies: null,
+    [Symbol(headers map)]: Map(1) { 'content-type' => [Object] },
+    [Symbol(headers map sorted)]: null
+  }
+*/
