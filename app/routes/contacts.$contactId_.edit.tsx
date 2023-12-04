b429b4ab9c16a1c63b02132f8179ec3cb8e16fb8
MS_D_rei <msmdmission1111@gmail.com>
2023-12-04 17:31:31 +0900

    ADD: investigation result of the contents of request, params, contents in action function

--- app/routes/contacts.$contactId_.edit.tsx
+++ app/routes/contacts.$contactId_.edit.tsx
@@ -5,8 +5,75 @@ import invariant from "tiny-invariant";
 
 import { getContact, updateContact } from "~/data";
 
-export const action = async ({ params, request }: ActionFunctionArgs) => {
+export const action = async ({ params, request, context }: ActionFunctionArgs) => {
   invariant(params.contactId, "Missing contactId");
+
+  // console.log("params: ", params);
+  // params:  { contactId: 'mttygb6' }
+  // console.log("request: ", request);
+//   request:  Request {
+//   size: 0,
+//   follow: 20,
+//   compress: true,
+//   counter: 0,
+//   agent: undefined,
+//   highWaterMark: 16384,
+//   insecureHTTPParser: false,
+//   [Symbol(Body internals)]: {
+//     body: ReadableStream {
+//       _state: 'readable',
+//       _reader: undefined,
+//       _storedError: undefined,
+//       _disturbed: false,
+//       _readableStreamController: [ReadableStreamDefaultController]
+//     },
+//     type: null,
+//     size: null,
+//     boundary: null,
+//     disturbed: false,
+//     error: null
+//   },
+//   [Symbol(Request internals)]: {
+//     method: 'POST',
+//     redirect: 'follow',
+//     headers: {
+//       accept: '*/*',
+//       'accept-encoding': 'gzip, deflate, br',
+//       'accept-language': 'en-US,en;q=0.9,ja-JP;q=0.8,ja;q=0.7',
+//       connection: 'keep-alive',
+//       'content-length': '50',
+//       'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+//       host: 'localhost:3000',
+//       origin: 'http://localhost:3000',
+//       referer: 'http://localhost:3000/contacts/mttygb6/edit',
+//       'sec-ch-ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+//       'sec-ch-ua-mobile': '?0',
+//       'sec-ch-ua-platform': '"macOS"',
+//       'sec-fetch-dest': 'empty',
+//       'sec-fetch-mode': 'cors',
+//       'sec-fetch-site': 'same-origin',
+//       'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+//     },
+//     credentials: 'same-origin',
+//     parsedURL: URL {
+//       href: 'http://localhost:3000/contacts/mttygb6/edit',
+//       origin: 'http://localhost:3000',
+//       protocol: 'http:',
+//       username: '',
+//       password: '',
+//       host: 'localhost:3000',
+//       hostname: 'localhost',
+//       port: '3000',
+//       pathname: '/contacts/mttygb6/edit',
+//       search: '',
+//       searchParams: URLSearchParams {},
+//       hash: ''
+//     },
+//     signal: AbortSignal { aborted: false }
+//   }
+// }
+  // console.log("context: ", context);
+  // context: {}
 
   const formData = await request.formData();
   const updates = Object.fromEntries(formData);
